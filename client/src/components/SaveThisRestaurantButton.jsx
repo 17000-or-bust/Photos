1ab7f73be8db5709c595afb0fd5441c387d78676
lydiahu8 <lydiahu8@gmail.com>
2019-02-15 13:43:42 -0800

    Fix button positioning and clean up code

--- client/src/components/SaveThisRestaurantButton.jsx
+++ client/src/components/SaveThisRestaurantButton.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class SaveThisRestaurantButton extends React.Component {
   constructor(props) {
@@ -29,8 +28,7 @@ class SaveThisRestaurantButton extends React.Component {
               <TextDiv>
                 <svg width="24px" height="24px">
                   <g id="icon/ic_bookmark" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                    <path d="M7,2.99987873 L17,2.99987873 C18.1045695,2.99987873 19,3.89530923 19,4.99987873 L19,20.6055572 C19,20.8816996 18.7761424,21.1055572 18.5,21.1055572 C18.421402,21.1055572 18.343911,21.0870278 18.273815,21.0514724 L12,17.8691561 L5.72618504,21.0514724 C5.4799129,21.1763909 5.17900339,21.0780144 5.05408484,20.8317423 C5.01852946,20.7616462 5,20.6841552 5,20.6055572 L5,4.99987873 C5,3.89530923 5.8954305,2.99987873 7,2.99987873 Z M7,5 L7,18.3374734 L12,15.7665154 L17,18.3374734 L17,5 L7,5 Z" id="Combined-Shape" fill="#333333">
-                    </path>
+                    <path d="M7,2.99987873 L17,2.99987873 C18.1045695,2.99987873 19,3.89530923 19,4.99987873 L19,20.6055572 C19,20.8816996 18.7761424,21.1055572 18.5,21.1055572 C18.421402,21.1055572 18.343911,21.0870278 18.273815,21.0514724 L12,17.8691561 L5.72618504,21.0514724 C5.4799129,21.1763909 5.17900339,21.0780144 5.05408484,20.8317423 C5.01852946,20.7616462 5,20.6841552 5,20.6055572 L5,4.99987873 C5,3.89530923 5.8954305,2.99987873 7,2.99987873 Z M7,5 L7,18.3374734 L12,15.7665154 L17,18.3374734 L17,5 L7,5 Z" id="Combined-Shape" fill="#333333" />
                   </g>
                 </svg>
                 <TextSpan>Save this restaurant</TextSpan>
@@ -41,8 +39,7 @@ class SaveThisRestaurantButton extends React.Component {
                 <svg width="24px" height="24px">
                   <g id="icon/ic_bookmarked" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
                     <g id="icon/ic_bookmark" transform="translate(5.000000, 3.000000)" fill="#DA3743">
-                      <path d="M2,-0.000121269196 L12,-0.000121269196 C13.1045695,-0.000121269196 14,0.895309231 14,1.99987873 L14,17.6055572 C14,17.8816996 13.7761424,18.1055572 13.5,18.1055572 C13.421402,18.1055572 13.343911,18.0870278 13.273815,18.0514724 L7,14.8691561 L0.726185039,18.0514724 C0.479912898,18.1763909 0.17900339,18.0780144 0.054084842,17.8317423 C0.0185294562,17.7616462 2.78518304e-15,17.6841552 2.77555756e-15,17.6055572 L0,1.99987873 C-6.25798147e-16,0.895309231 0.8954305,-0.000121269196 2,-0.000121269196 Z" id="Combined-Shape">
-                      </path>
+                      <path d="M2,-0.000121269196 L12,-0.000121269196 C13.1045695,-0.000121269196 14,0.895309231 14,1.99987873 L14,17.6055572 C14,17.8816996 13.7761424,18.1055572 13.5,18.1055572 C13.421402,18.1055572 13.343911,18.0870278 13.273815,18.0514724 L7,14.8691561 L0.726185039,18.0514724 C0.479912898,18.1763909 0.17900339,18.0780144 0.054084842,17.8317423 C0.0185294562,17.7616462 2.78518304e-15,17.6841552 2.77555756e-15,17.6055572 L0,1.99987873 C-6.25798147e-16,0.895309231 0.8954305,-0.000121269196 2,-0.000121269196 Z" id="Combined-Shape" />
                     </g>
                   </g>
                 </svg>
@@ -67,7 +64,7 @@ const SaveButton = styled.button`
   padding: 11px 15px;
   position: absolute;
   top: 10%;
-  left: 80%;
+  left: 85%;
   text-align: center;
   outline: none;
   font-size: 14px;
@@ -82,10 +79,6 @@ const ButtonSpan = styled.span`
   }
 `;
 
-const IconSpan = styled.span`
-  margin-right: 5px;
-`;
-
 const TextDiv = styled.div`
   display: flex;
   justify-content: center;
@@ -95,6 +88,5 @@ const TextDiv = styled.div`
 const TextSpan = styled.span`
   padding-top: 4px;
 `;
-
 
 export default SaveThisRestaurantButton;
