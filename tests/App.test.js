6e12b9f59a2c01de493a7cb5e08c776d4b30ce43
lydiahu8 <lydiahu8@gmail.com>
2019-02-15 21:59:44 -0800

    Add tests for App

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { configure, mount, shallow, render } from 'enzyme';
+import {
+  configure,
+  mount,
+  shallow,
+  render,
+} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import {
+  wrap,
+} from 'module';
 import App from '../client/src/components/App';
+import Photo from '../client/src/components/Photo';
+import PhotoBanner from '../client/src/components/PhotoBanner';
+import PhotoCarousel from '../client/src/components/PhotoCarousel';
+import PhotoCarouselLeftArrow from '../client/src/components/PhotoCarouselLeftArrow';
+import PhotoCarouselRightArrow from '../client/src/components/PhotoCarouselRightArrow';
+import PhotoDisplay from '../client/src/components/PhotoDisplay';
+import PhotoModal from '../client/src/components/PhotoModal';
+import SaveThisRestaurantButton from '../client/src/components/SaveThisRestaurantButton';
 
 configure({
   adapter: new Adapter(),
 });
 
-it('works', () => {
-  const wrap = shallow(<App />);
+describe('App', () => {
+  it('should render App without throwing an error', () => {
+    expect(shallow(<App />).exists()).toBe(true);
+  });
 
-  expect(wrap.text()).toEqual('Top Banner Photo Carousel50 Photos');
+  it('should render PhotoBanner without throwing an error', () => {
+    expect(shallow(<App />).find('PhotoBanner').exists()).toBe(true);
+  });
+
+  it('should render bookmark button without throwing an error', () => {
+    expect(shallow(<App />).find('SaveThisRestaurantButton').exists()).toBe(true);
+  });
 });
