14d52561e95fae053f724fb7e4354b3228d04487
lydiahu8 <lydiahu8@gmail.com>
2019-02-21 16:42:07 -0800

    Modify Dockerfile and connections for AWS EC2

--- client/src/lib/ajax.js
+++ client/src/lib/ajax.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 export default {
   getPhotos: (id, callback) => {
     $.get({
-      url: `http://localhost:8888/api/photos/${id}`,
+      url: `/api/photos/${id}`,
       success: data => callback(null, data),
       error: (err) => {
         callback(err);
