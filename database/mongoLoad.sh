9f8ea222b7ddc707f007c96195e4fbe93a47a336
Ryan Cowie <ryancowie@Ryans-Pro.local>
2019-03-02 20:00:09 -0800

    Adds shell script for mongo

--- database/mongoLoad.sh
+++ database/mongoLoad.sh
@@ -3,7 +3,18 @@
 echo -e "\n# -------------------------------------"
 echo "START Mongo Output"
 
+startTime=$(date +'%T')
+SECONDS=0
+echo "Starting at: $startTime"
+
+
 mongo restaurant --eval "printjson(db.dropDatabase())"
 
-mongoimport -d restaurant -c photos --type csv --file /Volumes/COWIE/photos/database/photos.csv
+mongoimport -d restaurant -c photos --type csv --file /Volumes/COWIE/photos/database/photos.csv --fields id,restaurant_id,image_url,caption,username,hover_data,date_posted
 
+
+endTime=$(date +'%T')
+echo "Finished at: $endTime"
+totalTime=$SECONDS
+echo -e "\nSuccess. Total time: $(($totalTime)) seconds\n"
+echo "FINISHED"
--- database/psqlLoad.sh
+++ database/psqlLoad.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 echo -e "\n# -------------------------------------"
 echo "START Postgres Output"
+
  /Library/PostgreSQL/11/bin/psql -d restaurant < /Volumes/COWIE/photos/database/psql.sql
+
 echo "END Postgres Output"
+
 startTime=$(date +'%T')
 SECONDS=0
+
 echo "Starting at: $startTime"
 echo "START Postgres Output"
+
  /Library/PostgreSQL/11/bin/psql -d restaurant < /Volumes/COWIE/photos/database/loadPsql.sql
-echo "END Postgres Output"
+
 endTime=$(date +'%T')
 echo "Finished at: $endTime"
+
 totalTime=$SECONDS
 echo -e "\nSuccess. Total time: $(($totalTime)) seconds\n"
 echo "FINISHED"
