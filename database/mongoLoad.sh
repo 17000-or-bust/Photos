3868a844cd9a0b9088daa6b3f98804c5dc3dd74c
Ryan Cowie <ryancowie@Ryans-Pro.local>
2019-03-02 17:56:52 -0800

    Saves files

--- database/mongoLoad.sh
+++ database/mongoLoad.sh
@@ -3,3 +3,7 @@
 echo -e "\n# -------------------------------------"
 echo "START Mongo Output"
 
+mongo restaurant --eval "printjson(db.dropDatabase())"
+
+mongoimport -d restaurant -c photos --type csv --file /Volumes/COWIE/photos/database/photos.csv
+
